82060bb6d4ce8e1a65fb3071089d0dd1e2016a2a
XXRadeonXFX <101211474+XXRadeonXFX@users.noreply.github.com>
2025-07-26 15:10:37 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ data "aws_vpc" "default" {
   default = true
 }
 
-data "aws_subnet" "default" {
+data "aws_subnets" "default" {
   filter {
     name   = "vpc-id"
     values = [data.aws_vpc.default.id]
@@ -11,6 +11,11 @@ data "aws_subnet" "default" {
   filter {
     name   = "default-for-az"
     values = ["true"]
+  }
+
+  filter {
+    name   = "availability-zone"
+    values = ["ap-south-1a", "ap-south-1b"]
   }
 }
 
@@ -27,18 +32,29 @@ resource "aws_security_group" "app_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = {
+    Name = "ecommerce-sg"
+  }
 }
 
 resource "aws_instance" "app" {
   ami                         = "ami-0f918f7e67a3323f0"
-  instance_type               = "t2.micro"
-  subnet_id                   = data.aws_subnet.default.id
+  instance_type               = "t3.micro" 
+  subnet_id                   = data.aws_subnets.default.ids[0] 
   vpc_security_group_ids      = [aws_security_group.app_sg.id]
   associate_public_ip_address = true
   key_name                    = var.key_name
